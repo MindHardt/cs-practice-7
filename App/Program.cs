c0b46c69236a2e13925758e1f471099301875153
ylnt241 <ylont34@gmail.com>
2025-12-06 13:24:03 +0500

    Now if <CTRL-C> pressed while writing to file program will delete it

--- App/Program.cs
+++ App/Program.cs
@@ -11,13 +11,20 @@ await Parallel.ForEachAsync(urls, cts.Token, async (url, ct) =>
 {
     try
     {
+        if (ct.IsCancellationRequested)
+            ct.ThrowIfCancellationRequested();
         using var http = new HttpClient();
         await using var content = await http.GetStreamAsync(url, ct);
         await content.CopyToAsync(destStream, ct);
         Console.WriteLine($"Successfully proceeded {url} to {destStream.Name}");
     }
-    catch
+    catch(Exception ex)
     {
+        if (ex is OperationCanceledException)
+        {
+            destStream.Close();
+            File.Delete(destStream.Name);
+        }
         Console.WriteLine($"Oops! Something went wrong when processing {url}");
     }
 });
