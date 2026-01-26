3861c1fe5a55c2350f16b882be8b613a4677eadf
Vadim Rudi <mrbrempy@gmail.com>
2026-01-26 19:10:34 +0500

    Исправил ошибки

--- App/Input.cs
+++ App/Input.cs
@@ -11,19 +11,13 @@ public static class Input
     public static string[] GetUris()
     {
         string[] result = [];
-        var valid = false;
-        while (valid is false)
+        while (true)
         {
             Console.Write("Введите нужные URL через пробел: ");
             result = Console.ReadLine()!.Split(' ');
-            valid = result.Any(x => IsValidUri(x) is false);
-            if (valid is false)
-            {
-                Console.WriteLine("Ошибка! Вы ввели некорректные URL!");
-            }
+            if (result.Length > 0 && result.All(x => IsValidUri(x))) return result;
+            Console.WriteLine("Ошибка! Вы ввели некорректные URL!");
         }
-
-        return result;
     }
 
     private static bool IsValidUri(string uri) => uri.StartsWith("https://");
@@ -37,15 +31,13 @@ public static class Input
         while (true)
         {
             Console.Write("Введите путь до файла с результатом: ");
-            try
+            var file = new FileInfo(Console.ReadLine()!);
+            if (file.Exists)
             {
-                var file = new FileInfo(Console.ReadLine()!);
-                return file;
+                Console.WriteLine("Файл уже существует. Перезаписать ? (y/n): ");
+                if (Console.ReadLine()!.ToLower() != "y") continue;
             }
-            catch
-            {
-                Console.WriteLine("Произошла ошибка, вероятно вы ввели некорректный путь. Попробуйте ещё раз.");
-            }
+            return file;
         }
     }
 }
