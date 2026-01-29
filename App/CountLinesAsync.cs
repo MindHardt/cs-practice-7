54bce701cd06ae9977954464bbb19a0f772dccd4
kotonW <katanjatsa@gmail.com>
2026-01-29 20:31:30 +0500

    Workable V1

---
+++ App/CountLinesAsync.cs
@@ -0,0 +1,17 @@
+﻿namespace App;
+
+public class CountLinesAsync
+{
+    public static async Task<int> CountLines(FileInfo filePath)
+    {
+        int countLines = 0;
+        using (StreamReader reader = new StreamReader(filePath.FullName))
+        {
+            while (await reader.ReadLineAsync() != null)
+            {
+                countLines++;
+            }
+        }
+        return countLines;
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,17 +1,56 @@
 ﻿using App;
 
 var cts = new CancellationTokenSource();
-Console.CancelKeyPress += (_, _) => cts.Cancel();
+Console.CancelKeyPress += (_, arg) =>
+{
+    Console.WriteLine("Получена команда отмены");
+    arg.Cancel = true;
+    cts.Cancel();
+};
 
 var uris = Input.GetUris();
-var dest = Input.GetOutputFile();
-var destStream = dest.OpenWrite();
+var destFile = Input.GetOutputFile();
+var destStream = destFile.OpenWrite();
 
-await Parallel.ForEachAsync(uris, cts.Token, async (uri, ct) =>
+using var http = new HttpClient();
+var semaphoreSlim = new SemaphoreSlim(3, 3);
+try
 {
-    using var http = new HttpClient();
-    await using var content = await http.GetStreamAsync(uri, ct);
-    await content.CopyToAsync(destStream, ct);
-});
-
-await destStream.DisposeAsync();
+    await Parallel.ForEachAsync(uris, cts.Token, async (uri, ct) =>
+    {
+        try
+        {
+            await using var content = await http.GetStreamAsync(uri, ct);
+            await semaphoreSlim.WaitAsync(ct);
+            try
+            {
+                await content.CopyToAsync(destStream, ct);
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+        catch (Exception e) when (e is not OperationCanceledException)
+        {
+            Console.WriteLine($"Ошибка при чтении файла {uri}: {e.Message}, файл игнорируется");
+        }
+    });
+}
+catch (OperationCanceledException)
+{
+    await destStream.DisposeAsync();
+    if (File.Exists(destFile.FullName))
+    {
+        destFile.Delete();
+    }
+    Console.WriteLine("Программа экстренно завершила работу. Файл удален.");
+    
+}
+finally
+{
+    await destStream.DisposeAsync();
+}
+Console.WriteLine($"Процесс завершен, данные загружены в {destFile.FullName}.");
+int countLines = await CountLinesAsync.CountLines(destFile);
+Console.WriteLine($"Количество строк в исходных файлах: {countLines}");
